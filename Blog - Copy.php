23faca4fd6ab0f776874fee74c3515392fcc0be0
Darshan Parmar <darshan@juniperwebs.com>
2014-12-07 16:44:20 +0530

    Hello
    I am here

---
+++ Blog - Copy.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+	
+	
+	  <link rel="canonical" href="http://ravimachines.com/" />
+      <meta property="og:url" content="http://ravimachines.com/" />
+        
+	<title>Ravi Machine Products List - Lathe Machine Manufacturer - Rajkot</title>
+	<meta name="description" content="Ravi Machine Products has 30 years of expertise in making reliable Lathe machine products. We are India’s Most Trusted Machine Product Manufacturers." />
+<?php include("include/metatags.php"); ?>
+	<?php include("include/analytics.php"); ?>
+	
+<link rel="shortcut icon" type="icon/png" href="http://caware.org/host/ravi/favicon.png" />
+	
+	
+	<link type="text/css" href="style.css" rel="stylesheet" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css' />
+	<script type="text/javascript" src="assets/js/jquery-1.8.0.min.js" ></script>
+	
+	<script type="text/javascript" src="assets/js/selectnav/selectnav.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/imagebox/imagebox.css" />
+	<script type="text/javascript" src="assets/js/imagebox/jquery.imagebox.min.js"></script>
+
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/carousel/carousel.css" />
+	<script type="text/javascript" src="assets/js/carousel/jquery.carousel.min.js"></script>
+
+	
+	<script type="text/javascript" src="assets/js/accordion/jquery.accordion.min.js"></script>
+	<script type="text/javascript" src="assets/js/tabify/jquery.tabify.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/css.validity.css" />
+	<script type="text/javascript" src="assets/js/jquery.validity.js"></script>
+	
+		
+	
+	<link rel="stylesheet" href="assets/css/fontawesome/font-awesome.min.css" />
+	<!--[if IE 7]>
+		<link rel="stylesheet" href="assets/css/fontawesome/font-awesome-ie7.min.css">
+	<![endif]-->
+	
+	<!-- /// FlexSlider ////////  -->
+	<link rel="stylesheet" type="text/css" href="assets/css/flexslider/flexslider.css" />
+	<script type="text/javascript" src="assets/js/flexslider/jquery.flexslider.min.js"></script>
+
+	<!-- /// Revolution Slider ////////  -->
+	
+	
+
+    <script type="text/javascript" src="assets/js/scripts.js"></script>
+	<script type="text/javascript" src="assets/js/plugins.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.tools.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/wide.css" name="layout">
+	<link rel="stylesheet" type="text/css" href="assets/css/skins/blue.css" name="skins">
+	
+	 <script type="text/javascript" src="assets/js/portfolio/jquery_002.js"></script>
+	<script type="text/javascript" src="assets/js/portfolio/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/portfolio/jquery_003.js"></script>
+    
+</head>
+</head>
+<body>
+
+	<div id="wrap">
+    	  <?php include_once("include/topbar.php"); ?>
+		    <?php include_once("include/menu.php"); ?>
+		<div id="content">
+		
+		<!-- /// CONTENT  /////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+		<div class="box">
+        
+        	<div class="row">
+            	<div class="span12">
+                	
+                    <h1>Welcome To Blog Of Lathe Machine</h1>
+   
+                    <div class="latest-article fixed">
+                    
+                    	<img class="responsive-img img-left" src="BLOG-IMG/Dril-Machine.jpg" alt="Lathe Machine" />
+                         
+                    	<h2>Exclusive Drill Machine</h2>
+                        
+                        <p>July 05, 2014 &#124; RAVI MACHINE PRODUCTS </p>
+                        
+                        <p align="justify">A Drill Machine is primarily used in design and technology for accurate drilling of holes.Drills are commonly used in woodworking, metalworking, construction, medicine, space missions and other applications. </p>
+                        
+						 <p align="justify">Banka Provides drill machine incorporating features such as Easy Power Feed by Hand Lever,Geometric Progression Speed & Feed and Easy Tapper Shank Release.</p>
+						 
+						<p align="justify"> <h3>Banka Drill Machine are design to give excellent performance which is available at Rs. 79000/- Only.</h3></p>
+						<p><a href="Drill-machine.php" class="button-blue">Read More</a></p> 
+                       
+                  
+                    
+                    </div>
+
+            	</div>
+            </div>
+            
+        </div>
+        
+        <div class="row">
+        	<div class="span8">
+ 
+                <div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Lathe-Machine.jpg" alt="Lathe Machine" />
+                
+                	<h3>What Is Lathe Machine?</h3>
+                    
+                    <p>July 01, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify">A Lathe is a machine tool which rotates the work-piece on its axis to perform various operations such as cutting, sanding, drilling, or deformation, facing, turning, with tools that are applied to the work-piece to create an object which has symmetry about an axis of rotation.</p>
+                    <p><a href="Blog-Lathe-Machine.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+                
+				<div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Ancient-Lathe-Machine.jpg" alt="Metal Working Lathe Machine" />
+                
+                	<h3>History Of Lathe Machine</h3>
+                    
+                     <p>July 10, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify">One of the first lathes in the UK was the Horizontal Boring Machine that was installed by Jan Verbruggen in 1772 in the Royal Arsenal in Woolwich.During the Industrial Revolution, mechanized power generated by water wheels or steam engines was transmitted to the lathe via line shafting, allowing faster and easier work.</p>
+                     <p><a href="Blog-Histoy-Lathe-Machine.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+               <div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Lathe-Machine-Types.jpg" alt="Category Of Lathe Machine" />
+                
+                	<h3>Major Categories Of Lathe</h3>
+                    
+                   <p>July 20, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify"><ul><li>WoodWorking Lathes</li>
+					<li>MetalWorking lathes</li>
+					<li>Watchmaker's lathes</li>
+					<li>Ornamental Turning Lathes</li>
+					
+					</ul></p>
+                   <p><a href="Blog-Lathe-Categories.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+                
+            <div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Lathe-Machine-Parts.jpg" alt="Lathe-Machine-parts" />
+                
+                	<h3>Parts Of Lathe Machine</h3>
+                    
+                   <p>July 30, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify"><ul>
+					<li>Lathe Bed</li>
+					<li>Headstock</li>
+					<li>Tailstock</li>
+					
+					</ul></p>
+                   <p><a href="Blog-Lathe-Parts.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+				
+				<div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Lathe-Machine-Accessories.jpg" alt="Lathe-Machine-Accessories" />
+                
+                	<h3>Accessories Incorporated In Lathe Machine</h3>
+                    
+                   <p>August 01, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify">Unless a workpiece has a taper machined onto it which perfectly matches the internal taper in the spindle, or has threads which perfectly match the external threads on the spindle (two conditions which rarely exist), an accessory must be used to mount a workpiece to the spindle.
+					
+					</p>
+                   <p><a href="Blog-Accessories-Lathe-Machine.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+				
+				<div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Safety-Lathe-Machine.jpg" alt="Lathe-Machine-Accessories" />
+                
+                	<h3>Safety Measure While Operating Lathe Machine</h3>
+                    
+                   <p>August 10, 2013 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify">All lathe operators must be constantly aware of the safety hazards that are associated with using the lathe and must know all safety precautions to avoid accidents and injuries. Carelessness and ignorance are two great menaces to personal safety. Other hazards can be mechanically related to working with the lathe, such as proper machine maintenance and set-up.
+					
+					</p>
+                   <p><a href="Blog-Safety-Measures.php" class="button-blue">Read More</a></p> 
+                
+                </div>
+				
+				<div class="article fixed">
+                
+                	<img class="responsive-img" src="BLOG-IMG/Testing-Lathe-Machine.jpg" alt="Lathe-Machine-Accessories" />
+                
+                	<h3>How to test Lathe Machine?</h3>
+                    
+                   <p>September 01, 2014 &#124; RAVI MACHINE PRODUCTS </p>
+                    
+                    <p align="justify">Optimum strategies are required for testing the lathe machine with respect to its price/performance ratio. Some of the tests includes:<ul>
+					
+					<li>ACCURACY TEST</li>
+					<li>HEAD & TAIL TEST</li>
+					<li>ELECTRICAL TESTING</li>
+					<li>MACHINE NOISE TEST</li>
+					</ul>
+					
+					</p>
+                   <p><a href="Lathe-Machine-Test-Report-Image.php" class="button-blue">Read More</a></p> 
+                
+                </div> 
+
+            </div>
+			<div class="span4">
+              	<div class="left-navigation">
+				<h5 class="text-uppercase">Lathe Machine Models:</h5><br />
+                    
+                    <ul class="categories-list">
+                    	<li><a title="Lathe Machine" href="Lathe-Machine-Banka34.php">Banka 34 THINDERBOLT - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka35.php">Banka 35 CYCLONE - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka40.php">Banka 40 SPEEDJET - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka43.php">Banka 43 GLADIATOR - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka50.php">Banka 50 ACHIEVER - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka60.php">Banka 60 POWERHOUSE - Lathe Machine</a></li>
+                    </ul>
+					
+                    <h5 class="text-uppercase">Company Profile:</h5><br />
+                    
+                    <ul class="categories-list">
+                    	<li><a title=" Features Of Lathe Machine" href="process/download.php?file=Featuers">Features of Lathe Machine</a></li>
+                        <li><a title=" Accessories Of Lathe Machine " href="process/download.php?file=ACCESSORIES">Accessories of Lathe Machine</a></li>
+                        <li><a title="Technical Specifications:Lathe Machine" href="process/download.php?file=Specs">Technical Specifications of Lathe Machine</a></li>
+                        <li><a title="Our Brochure Of Lathe Machine " href="process/download.php?file=catalog">Our Brochure / Catalogue</a></li>
+                        <li><a title="Terms & Condition: Lathe Machine" href="process/download.php?file=generalterms">General Terms</a></li>
+                        <li><a title="Know About Lathe Machine" href="process/download.php?file=newcustomer">I am a New Customer</a></li>
+						<li><a title="FAQ Of Lathe Machine" href="process/download.php?file=inspection">FAQ for Lathe Machine Inspection</a></li>
+                    </ul>
+  
+                    <h5 class="text-uppercase">Our Vision</h5>
+                    
+                    <div class="text-widget">
+                    
+                    	<p align="justify">To be the leading company of lathe machine that is incorporated with superior quality, cost effective, longer durability, advance technology and to provide after sale service support to our customer with product. </p>
+                    
+                    </div>
+                    
+                    <h5 class="text-uppercase"> Product Portfolio</h5>
+                    
+                    <ul class="tags-list">
+                    	<li><a title="Lathe Machine"  href="Lathe-Machine-Banka34.php">BANKA 34 THINDERBOLT - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka35.php">BANKA 35 CYCLONE - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine " href="Lathe-Machine-Banka40.php">BANKA 40 SPEEDJET - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka43.php">BANKA 43 GLADIATOR - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Lathe-Machine-Banka50.php">BANKA 50 AHIEVER - Lathe Machine</a></li>
+                        <li><a title="Lathe Machine" href="Drill-machine.php">BANKA 60 POWERHOUSE - Lathe Machine</a></li><br/>
+                        <li><a title="Drill Machine" href="Drill-machine.php">Drill Machine</a></li>
+                    </ul>
+                
+                </div>
+            
+            </div> 
+	
+        </div>
+
+
+		</div><!-- end #content -->
+  <br />
+
+	<?php
+	include("include/footer.php");
+	?>
+
+<div class="switcher"></div>
+
+</body>
+
+</html>
---
+++ contact - Copy.php
@@ -0,0 +1,219 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="ie6" xmlns="http://www.w3.org/1999/xhtml"><![endif]-->
+<!--[if IE 7]><html class="ie7" xmlns="http://www.w3.org/1999/xhtml"><![endif]-->
+<!--[if IE 8]><html class="ie8" xmlns="http://www.w3.org/1999/xhtml"><![endif]-->
+<!--[if IE 9]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+
+	<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+	
+	
+	  <link rel="canonical" href="http://ravimachines.com/" />
+      <meta property="og:url" content="http://ravimachines.com/" />
+        
+	<title>Ravi Machine Products List - Lathe Machine Manufacturer - Rajkot</title>
+	<meta name="description" content="Ravi Machine Products has 30 years of expertise in making reliable Lathe machine products. We are India’s Most Trusted Machine Product Manufacturers." />
+<?php include("include/metatags.php"); ?>
+	<?php include("include/analytics.php"); ?>
+	
+<link rel="shortcut icon" type="icon/png" href="http://caware.org/host/ravi/favicon.png" />
+	
+	
+	<link type="text/css" href="style.css" rel="stylesheet" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css' />
+	<script type="text/javascript" src="assets/js/jquery-1.8.0.min.js" ></script>
+	
+	<script type="text/javascript" src="assets/js/selectnav/selectnav.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/imagebox/imagebox.css" />
+	<script type="text/javascript" src="assets/js/imagebox/jquery.imagebox.min.js"></script>
+
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/carousel/carousel.css" />
+	<script type="text/javascript" src="assets/js/carousel/jquery.carousel.min.js"></script>
+
+	
+	<script type="text/javascript" src="assets/js/accordion/jquery.accordion.min.js"></script>
+	<script type="text/javascript" src="assets/js/tabify/jquery.tabify.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/css.validity.css" />
+	<script type="text/javascript" src="assets/js/jquery.validity.js"></script>
+	
+		
+	
+	<link rel="stylesheet" href="assets/css/fontawesome/font-awesome.min.css" />
+	<!--[if IE 7]>
+		<link rel="stylesheet" href="assets/css/fontawesome/font-awesome-ie7.min.css">
+	<![endif]-->
+	
+	<!-- /// FlexSlider ////////  -->
+	<link rel="stylesheet" type="text/css" href="assets/css/flexslider/flexslider.css" />
+	<script type="text/javascript" src="assets/js/flexslider/jquery.flexslider.min.js"></script>
+
+	<!-- /// Revolution Slider ////////  -->
+	
+	
+
+    <script type="text/javascript" src="assets/js/scripts.js"></script>
+	<script type="text/javascript" src="assets/js/plugins.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.tools.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/wide.css" name="layout">
+	<link rel="stylesheet" type="text/css" href="assets/css/skins/blue.css" name="skins">
+	
+	 <script type="text/javascript" src="assets/js/portfolio/jquery_002.js"></script>
+	<script type="text/javascript" src="assets/js/portfolio/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/portfolio/jquery_003.js"></script>
+    
+	
+	
+	
+</head>
+
+<body id="index">
+	<div id="wrap">
+		<?php include_once("include/topbar.php"); ?>
+		<?php include_once("include/menu.php"); ?>
+		
+		<div id="content">
+			</br></br></br>
+            <div class="row">
+				<div>
+					<p style="text-align:justify;">
+						<h2>Feel free to say "Hello" to us, Anytime ! </h2>
+					Feel free to make any inquiries you may have regarding Our Best Quality Lathe Machines  Products Like Lathe Machine,Pillar Drill, Centre Lathe Machine, All Geared Lathe Machine, Drill Machine, Accessories,etc.
+					</p>
+				</div>
+				
+				<?php
+					if(isset($_SESSION["message"])){
+				?>
+						<div class="alert alert-success" id="alert">
+							<p>
+								<i class="icon-ok icon-2x"></i> <?php echo $_SESSION["message"]; ?>
+								<span class="pull-right">
+									<b><i class="icon-remove"></i></b>
+								</span>
+							</p>
+						</div>
+				<?php
+					unset($_SESSION["message"]); }
+				?>
+                
+				<div class="span6" style="width:575px;margin-left: 0;">                
+                    <div class="contact span6" style="margin-right:25px; margin-top:12px;">
+						<h1 style="color:#FFF;">RAVI MACHINE PRODUCTS </h1>
+						<ul class="location">
+
+							<li class="address"><a style="color:#FFFFFF;" href="http://www.hotelkrishnapark.in/" target="_blank"> Preferred Hotel Near Our Premises</a>
+							</li>
+						</ul>
+						<ul class="location">
+						<li class="bank"><a style="color:#FFFFFF;" href="ravi-payment.php">Our Bank Details</a></li>
+							<li class="address">
+								2, Samrat Industrial Area,<br />Gondal Road,<br />Rajkot - 360 004. Gujarat (INDIA) <br/> (WEDNESDAY OFF) <br/>(Sunday OPEN) <br/>
+								<a style="color:#FFF;" target="_blank" href="http://goo.gl/maps/5OvMg"> Locate us On Google Map</a>
+							</li>
+							
+							<li class="admin"><a style="color:#FFFFFF;" href="process/download.php?file=read before">Read before you Visit</a></li>
+							<li class="address"> 
+								Work Address<br />Essen Road,<br />Opposite Essen Multipack <br/> Veraval - Sapar <br/>Rajkot - 360024<br/>
+								
+							</li>
+							
+							<li class="phone">
+								Sales : +91-7817866066 <br />After Sales : 09377093780<br />Customer Care : +91-9374102898
+							</li>
+							
+							<li class="email">
+								Email: sales@bankaindia.com
+							</li>
+							
+							<li>
+								<script src="https://apis.google.com/js/platform.js"></script>
+								<div id="placeholder-div1"></div>
+								
+								<script>
+									gapi.hangout.render('placeholder-div1', {
+										'render': 'createhangout',
+										'initial_apps': [{'app_id' : '184219133185', 'start_data' : 'dQw4w9WgXcQ', 'app_type' : 'ROOM_APP' }]
+									});
+								</script>
+							</li>
+						</ul>
+						<!--	<li><a style="color:#FFFFFF;" href="http://www.redbus.in/" target="_blank"><img src="assets/images/imgres.jpg" style=" height:30px; width:50px; padding-left:45px;" /></a>
+							</li>
+							<ul class="location">
+
+							<li class="address"><a style="color:#FFFFFF;" href="http://www.hotelkrishnapark.in/" target="_blank"> Preferred Hotel Near Our Premises</a>
+							</li>
+							</ul>
+							<ul class="checklist last">
+							<li style="color:#FFFFFF; margin-left:24px;"> How to Check Machines
+							</li>
+							</ul>
+							<ul class="location categories-list">
+							<li><a style="color:#FFFFFF;" href="process/download.php?file=FAQ for Machine Inspection">Day1</a></li>
+							<li><a style="color:#FFFFFF;" href="process/download.php?file=inspection1">Day2</a>
+							</li>
+						</ul> -->
+					</div><!-- end. contact -->
+                    
+					<div class="span2">
+						<h3>Send us a message</h3>
+                    
+                        <script type="text/javascript">
+							jQuery(document).ready(function(){
+								jQuery("#contactform").validate();
+							});
+						</script>
+
+						<form method="post" class="fixed" action="process/AddLead.php">
+							<div>
+								<input type="text" size="28" name="Name" id="contactname" value="" class="required" placeholder="Name*" />
+							</div>
+							
+							<input type="hidden" name="date" value="<?php echo date("d M Y h:i A");?>">
+							
+							<div>
+								<input type="text" size="28" name="Email" id="email" value="" class="required email" placeholder="E-mail*" />
+							</div>
+							
+							<div>
+								<input type="text" size="28" name="Contact" id="email" value="" class="required email" placeholder="Contact-no*" />
+							</div>
+							
+							<input type="hidden" name="source" value="4">
+							
+							<div>
+								<textarea rows="12" cols="38" name="message" id="message" placeholder="Enter a message"></textarea>
+							</div>
+							
+							<div>
+								<input type="submit" value="Send Message" class="btn btn-blue" />
+							</div>
+						</form>
+                    </div>
+				</div><!-- end .span8 -->
+				
+				<div class="span6">
+					<div class="left-navigation">
+						<h3>Locate us on Google Maps </h3>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14769.839511337725!2d70.796049!3d22.260563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959ca63531dd665%3A0x9e03d6adebba3c83!2sRavi+Machine+Products%2C+Rajkot%2C+Gujarat!5e0!3m2!1sen!2sin!4v1387889279158" width="100%" height="500" frameborder="0" style="border:0"></iframe>
+					</div><!-- end .left-navigation -->
+				</div><!-- end .span4 -->
+			</div><!-- end .row -->
+		</div><!-- end #content -->
+		</br>
+		<?php include("include/footer.php");?>
+		<script>
+			$(document).ready(function(){
+				$("#alert").fadeOut(4000);
+			});
+		</script>
